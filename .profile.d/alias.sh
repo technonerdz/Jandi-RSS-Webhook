bb5336d03a8e8481aa0acf4bd59ae5b35ad5f85d
jsuch2362 <jsuch2362@gmail.com>
2016-03-03 13:37:09 +0900

    modify shell

--- .profile.d/alias.sh
+++ .profile.d/alias.sh
@@ -1,3 +1,6 @@
 function rss() {
-    command curl -v http://$HEROKU_APP_NAME.herokuapp.com/rss/harverst
+    command curl -v http://$HEROKU_APP_NAME.herokuapp.com/rss/harvest
+}
+function rss_localhost() {
+    command curl -v http://localhost/rss/harvest
 }
